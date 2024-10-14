5c63cf2935802ade45a71a03d4b7e3c67da348b4
bekturich <bekosaa@mail.ru>
2024-10-14 09:51:14 +0600

    newproject

--- mykino/mykino/settings.py
+++ mykino/mykino/settings.py
@@ -145,6 +145,7 @@ LANGUAGES = (
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
